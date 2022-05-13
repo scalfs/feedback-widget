53f5709889f4308c1a67f6fbc40e18bb9a3b4845
Vitor Escalfoni <vitor.hme@gmail.com>
2022-05-13 15:20:32 -0300

    chore: added reanimated and gesture libraries, and started widget development

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import Widget from "./src/components/Widget";
+import { theme } from "./src/theme";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import { useCallback, useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  useEffect(() => {
+    // Keep the splash screen visible while we fetch resources
+    SplashScreen.preventAutoHideAsync();
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    // we hide the splash screen once we know the root view has already performed layout.
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{ flex: 1, backgroundColor: theme.colors.background }}
+      onLayout={onLayoutRootView}
+    >
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+
+      <Widget />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,29 @@
+import { ChatTeardropDots } from "phosphor-react-native";
+import { useRef } from "react";
+import { TouchableOpacity, View } from "react-native";
+import { theme } from "../../theme";
+import { styles } from "./styles";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleOpen = () => bottomSheetRef.current?.expand();
+  return (
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
+        <ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      ></BottomSheet>
+    </>
+  );
+}
+
+export default gestureHandlerRootHOC(Widget);
